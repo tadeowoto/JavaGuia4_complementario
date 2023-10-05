8ace60e41d375e3631fabdf52890646fcf31ecd9
Tadeo <tadeo.woto@gmail.com>
2023-10-05 18:43:41 -0300

    finished TrabajoPintura class

--- ejComplementario/src/TrabajoPintura.java
+++ ejComplementario/src/TrabajoPintura.java
@@ -12,18 +12,58 @@ public class TrabajoPintura extends Servicio {
     
     protected String edificacion;
     protected double superficie, precioLitroPintura;
+    protected boolean costeAdicional=false;
 
-    public TrabajoPintura(String edificacion, double superficie, double precioLitroPintura, String trabajador, String cliente, LocalDate fechaInicio) {
+    public TrabajoPintura(String edificacion, double superficie, double precioLitroPintura, boolean costeAdicional, String trabajador, String cliente, LocalDate fechaInicio) {
         super(trabajador, cliente, fechaInicio);
         this.edificacion = edificacion;
         this.superficie = superficie;
         this.precioLitroPintura = precioLitroPintura;
+        this.costeAdicional = costeAdicional;
     }
-    
-    
-    
-        
-    
+
+    public String getEdificacion() {
+        return edificacion;
+    }
+
+    public void setEdificacion(String edificacion) {
+        this.edificacion = edificacion;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+
+    public double getPrecioLitroPintura() {
+        return precioLitroPintura;
+    }
+
+    public void setPrecioLitroPintura(double precioLitroPintura) {
+        this.precioLitroPintura = precioLitroPintura;
+    }
+
+    public boolean isCosteAdicional() {
+        return costeAdicional;
+    }
+
+    public void setCosteAdicional(boolean costeAdicional) {
+        this.costeAdicional = costeAdicional;
+    }
+
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+
     
     
     
@@ -31,38 +71,55 @@ public class TrabajoPintura extends Servicio {
     
     
     @Override
-    protected double costeMaterial(){
-        
-        
-        
-        return 0;
-        
-        
-        
+    protected double costeMaterial() {
+
+        return (superficie / 7.8) * precioLitroPintura;
+
     }
-    
-    
-    
+
     @Override
-    protected double costeManoObra(){
-        
-        return 0;
-        
+    protected double costeManoObra() {
+
+        return (superficie / 10) * 9.5;
+
     }
-    
-    
+
+
     @Override
-    protected double costeTotal(){
-        
-        
-        return 0;
-        
+    protected double costeTotal() {
+
+        if (superficie < 50) {
+
+            System.out.println("la superficie es menos de 50mts cuadrados, se la añade un costo de un 15%");
+            costeAdicional=true;
+            return (costeMaterial() + costeManoObra()) * 0.15;
+            
+            
+            
+
+        } else {
+            return costeMaterial() + costeManoObra();
+        }
+
     }
+    
+    
+    
     
     
     @Override
     protected String detalleServicio(){
         
+        System.out.println("TRABAJO DE PINTURA");
+        System.out.println("Cliente: "+super.getCliente());
+        System.out.println("Fecha de inicio: "+super.fechaInicio);
+        System.out.println("-----------------------------------");
+        System.out.println("");
+        System.out.println("Pintor: "+super.getTrabajador());
+        System.out.println("Coste material "+costeManoObra());
+        System.out.println("Coste adicional "+ costeAdicional);
+        System.out.println("-----------------------------------");
+        System.out.println("TOTAL "+costeTotal());
         
         
         return null;
